00a1a774b02067e508c0e33abbd2a325911b3312
kira0x1 <kira.eva@protonmail.com>
2021-04-25 06:11:48 +0300

    fixed wasm content-type

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ app.use(
     express.static("public", {
         setHeaders: function (res, path) {
             if (path.endsWith(".br")) {
-                if (path.endsWith(".wasm.br"))
-                    res.set("Content-Type", "application/wasm");
-                else
-                    res.set("Content-Type", "application/javascript");
-
+                setContentHeaders(res, path)
                 res.set("Content-Encoding", "br");
             }
         },
     })
 );
+
+/**
+ * 
+ * @param {string} res 
+ * @param {string} path 
+ */
+function setContentHeaders(res, path) {
+    if (path.includes(".wasm.br"))
+        res.set("Content-Type", "application/wasm");
+    else
+        res.set("Content-Type", "application/javascript");
+}
 
 app.listen(PORT, () => {
     console.log(`listening on https://localhost:${PORT}`);
