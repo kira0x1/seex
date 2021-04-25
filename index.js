3cc89296d958155b9cff5b4209d856a10c42aa09
kira0x1 <kira.eva@protonmail.com>
2021-04-25 04:34:08 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public', {
+    setHeaders: function (res, path) {
+        if (path.endsWith('.br')) {
+            res.set("Content-Type", "wasm");
+            res.set("Content-Encoding", "br");
+        }
+    }
+}))
+
+app.listen(PORT, () => {
+    console.log(`listening on https://localhost:${PORT}`);
+})
