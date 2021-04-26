1ca15da4a883daa0fa996ff5bbbaef4271f1c47c
kira0x1 <kira.eva@protonmail.com>
2021-04-26 06:42:07 +0300

    Formatted index.js

--- index.js
+++ index.js
@@ -3,28 +3,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(
-    express.static("public", {
-        setHeaders: function (res, path) {
-            if (path.endsWith(".br")) {
-                setContentHeaders(res, path)
-                res.set("Content-Encoding", "br");
-            }
-        },
-    })
+  express.static("public", {
+    setHeaders: function (res, path) {
+      if (path.endsWith(".br")) {
+        setContentHeaders(res, path);
+        res.set("Content-Encoding", "br");
+      }
+    },
+  })
 );
 
 /**
- * 
- * @param {string} res 
- * @param {string} path 
+ *
+ * @param {string} res
+ * @param {string} path
  */
 function setContentHeaders(res, path) {
-    if (path.endsWith(".wasm.br"))
-        res.set("Content-Type", "application/wasm");
-    else
-        res.set("Content-Type", "application/javascript");
+  if (path.endsWith(".wasm.br")) res.set("Content-Type", "application/wasm");
+  else res.set("Content-Type", "application/javascript");
 }
 
 app.listen(PORT, () => {
-    console.log(`listening on https://localhost:${PORT}`);
+  console.log(`listening on https://localhost:${PORT}`);
 });
